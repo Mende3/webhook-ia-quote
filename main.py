3408ad105f5d913f84fb17354268f2c662ac56dc
dell@SMALL <devsmalldev@gmail.com>
2025-08-18 17:47:14 +0100

    ++flask cors

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import os
 from flask import Flask, request, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/webhookcallback", methods=["POST"])
 def hook():
-    print(request.data)  # Isso vai para os logs do Render
+    print(request.data)
     return "Hello world", 200
 
-@app.route("/")  # Rota básica para testar se está vivo
+@app.route("/") 
 def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render define a porta
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
