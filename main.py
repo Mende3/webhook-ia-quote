2466a3857f40a4eed673d6f4098f6b3356f07df1
Mende3 <smallcoin8@gmail.com>
2025-08-18 12:40:46 +0100

    Update main.py
    new

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import os
 from flask import Flask, request
 
-
 app = Flask(__name__)
 
 @app.route("/webhookcallback", methods=["POST"])
 def hook():
-    print (request.data)
+    print(request.data)  # Isso vai para os logs do Render
     return "Hello world", 200
 
-@app.route("/")
+@app.route("/")  # Rota básica para testar se está vivo
 def home():
-    return "API em flask rodando"
+    return "API Flask no Render está rodando!"
 
 if __name__ == "__main__":
-    port = int(os.envioron.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))  # Render define a porta
     app.run(host="0.0.0.0", port=port)
+
